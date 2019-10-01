2ff95f21575c5f3659f1f2c85ec362b82d43ec96
Raghavprasanna Rajagopalan <raghavp96@gmail.com>
2019-10-01 13:30:35 -0400

    can query sql, not neo4j yet

--- data/setup.sh
+++ data/setup.sh
@@ -7,11 +7,7 @@ DISGENET_DB_FILE_NAME="disgenet_2018.db"
 
 # download disgenet into data/sources
 wget  -P "${SCRIPT_DIR}/sources/" "http://www.disgenet.org/static/disgenet_ap1/files/current/${DISGENET_DB_FILE_NAME}.gz"
-gunzip "${DISGENET_DB_FILE_NAME}.gz"
+gunzip "${SCRIPT_DIR}/sources/${DISGENET_DB_FILE_NAME}.gz"
 
-# set-up local neo4j db
-
-# run data.py - connect to sqllite db and neo4j, pull all *genes and diseases and associations* and add the ones not in neoj into neo4j
-
-
-
+# install python requirements
+pip install -r requirements.txt
