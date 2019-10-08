e3e4d340c7e56b978c9aaffd92e9cea6e80a061d
Raghavprasanna Rajagopalan <raghavp96@gmail.com>
2019-10-08 08:49:38 -0400

    Moved setup file out to root dir, create process documentation

---
+++ doc/process.md
@@ -0,0 +1,4 @@
+# This doc serves as a log of our research process
+
+## 10/1 - Exploring Disgenet
+
--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ DISGENET_DB_FILE_NAME="disgenet_2018.db"
 # install sqllite3, neo4j, wget, gunzip
 
 # download disgenet into data/sources
-wget  -P "${SCRIPT_DIR}/sources/" "http://www.disgenet.org/static/disgenet_ap1/files/current/${DISGENET_DB_FILE_NAME}.gz"
-gunzip "${SCRIPT_DIR}/sources/${DISGENET_DB_FILE_NAME}.gz"
+wget  -P "${SCRIPT_DIR}/data/sources/" "http://www.disgenet.org/static/disgenet_ap1/files/current/${DISGENET_DB_FILE_NAME}.gz"
+gunzip "${SCRIPT_DIR}/data/sources/${DISGENET_DB_FILE_NAME}.gz"
 
 # install python requirements
 pip install -r requirements.txt
