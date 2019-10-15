f82300c1ca58c1f57450bba73f971bef0f72d8f8
Raghavprasanna Rajagopalan <raghavp96@gmail.com>
2019-10-14 23:56:18 -0400

    update script rq

--- data/brute_export.sh
+++ data/brute_export.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -rf ../csv
-mkdir ../csv
+rm -rf csv
+mkdir csv
 
 sqlite3 -header -csv "sources/disgenet_2018.db" < "sql/export_gene.sql" > "csv/gene.csv"
 sqlite3 -header -csv "sources/disgenet_2018.db" < "sql/export_disease.sql" > "csv/disease.csv"
